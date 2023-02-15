8a549d5932432c365ad2724733a7eb908fc41920
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-15 18:52:27 +0800

    Create Casting in Python.py

---
+++ Casting in Python.py
@@ -0,0 +1,29 @@
+
+# Integers
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+# Floats
+
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+# Strings
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
