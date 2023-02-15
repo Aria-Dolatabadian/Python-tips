68ae2a9ed1ee56f263f5e8caf5c614887c639ae4
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-15 19:03:28 +0800

    Create Example for input if else.py

---
+++ Example for input if else.py
@@ -0,0 +1,21 @@
+
+x=input('Year of Birth:')
+print('Age:',(2020-int(x)),('Years old'))
+z=(2020-int(x))
+if z>=50:
+    print('Direction:Go right')
+else:
+    print('Direction:Go left')
+y=input('Gender:')
+z='male'
+if y=='male':
+    print('Enter the first room')
+else:
+    print('Enter the second room')
+u=input('How tall are you? (cm) ')
+print('Height:', int(u), ('cm'))
+w=int(u)
+if w>=180:
+    print('Please seat at back')
+else:
+    print('Please seat at front')
