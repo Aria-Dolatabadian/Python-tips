d294ef809da604b4b69b3d791eafcf954dd698a4
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-15 18:59:04 +0800

    Create Input calculator.py

---
+++ Input calculator.py
@@ -0,0 +1,17 @@
+
+x = int(input('Addition:1 Subtraction:2 Multiplication:3 Division:4:'))
+y = int(input("insert  a number:"))
+z= int (input ("insert another number:"))
+
+if x==1:
+    print('Addition')
+    print(y+z)
+elif x==2:
+    print('Subtraction')
+    print(y-z)
+elif x==3:
+    print('Multiplication')
+    print(y*z)
+elif x==4:
+    print('Division')
+    print(y/z)
