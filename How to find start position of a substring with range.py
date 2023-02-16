a6102d2177afcadcf29d9f664fd2335f20500c4d
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-16 11:38:30 +0800

    Create How to find start position of a substring with range.py

---
+++ How to find start position of a substring with range.py
@@ -0,0 +1,13 @@
+s = 'abcd1234dcba'
+
+print(s.find('a'))  
+print(s.find('cd'))  
+print(s.find('1', 0, 5))  
+print(s.find('1', 0, 2)) 
+
+print('...............')
+
+print(s.rfind('a'))  
+print(s.rfind('cd'))  
+print(s.rfind('1', 0, 5))  
+print(s.rfind('1', 0, 2)) 
