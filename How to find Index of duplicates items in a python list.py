e455d52ab548f64170fca2a73d9908058014084e
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-16 11:38:47 +0800

    Create How to find Index of duplicates items in a python list.py

---
+++ How to find Index of duplicates items in a python list.py
@@ -0,0 +1,17 @@
+DNA = "UTCAAAUTCAAAAUTC"
+
+def list_duplicates_of(seq,item):
+    start_at = -1
+    locs = []
+    while True:
+        try:
+            loc = seq.index(item,start_at+1)
+        except ValueError:
+            break
+        else:
+            locs.append(loc)
+            start_at = loc
+    return locs
+
+
+print(list_duplicates_of(DNA, 'UTC'))
