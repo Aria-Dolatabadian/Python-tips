0037b76880a2cab7d9928b9b9caa3176260cc613
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-16 11:21:45 +0800

    Create Changing a parameter in a list.py

---
+++ Changing a parameter in a list.py
@@ -0,0 +1,17 @@
+def change(my_list):
+    print('Before', my_list)
+    my_list[2]=10
+    print('After', my_list)
+
+x=[1,2,3,4,5]
+print(x)
+change(x)
+print(x,'*')
+print('...........................')
+def change(my_list):
+    my_list=[10,20,30]
+    print(my_list)
+
+my_list=[1,2,3,4,5]
+change(my_list)
+print(my_list,'*')
