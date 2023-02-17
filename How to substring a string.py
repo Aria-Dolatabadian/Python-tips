345ce038913bfb314ae97fee72bffe0f3e5145ab
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-17 12:59:10 +0800

    Create How to substring a string.py

---
+++ How to substring a string.py
@@ -0,0 +1,15 @@
+DNA="ATACAGATAGACATGATGAATGAATGATATTATATATAGGAATACCGAGTATTAGATGAGAG"
+#  Get the first 5 characters of a string
+print(DNA[0:5])
+#  Get a substring of length 4 from the 3rd character of the string
+print(DNA[2:6])
+#  Get the last character of the string
+print(DNA[-1])
+#  Get the last 5 characters of a string
+print(DNA[-5:])
+#  Get a substring which contains all characters except the last 4 characters and the 1st character
+print(DNA[1:-4])
+#  Get every other character from a string
+print(DNA[::2])
+#  More examples
+print (DNA[-5:-2])
