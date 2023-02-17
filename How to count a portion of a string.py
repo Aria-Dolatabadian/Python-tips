f939e8ea3776f35f0128daa9b519ff9a63657667
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-17 12:49:45 +0800

    Create How to count a portion of a string.py

---
+++ How to count a portion of a string.py
@@ -0,0 +1,10 @@
+DNA="ATCATACAGATGATACATGACATGACATGACATGAG"
+c=DNA.count("C")
+print(c)
+
+# and start point
+
+DNA="aaaaaTTaaaaaaaaaaaaaaaaaaaaaaa"
+TT=DNA.count("TT")
+print("number of TT:",(TT))
+print(("starts:"), (DNA.find("T")))
