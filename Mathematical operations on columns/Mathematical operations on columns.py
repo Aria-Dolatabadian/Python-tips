fd7a4f56fd62caf1aabf6f5f6fb471477ae4cfca
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-17 13:01:23 +0800

    Update Mathematical operations on columns.py

--- Mathematical operations on columns/Mathematical operations on columns.py
+++ Mathematical operations on columns/Mathematical operations on columns.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_excel (r'D:\python\Data.xlsx', sheet_name='new')
+df = pd.read_excel (r'Data.xlsx', sheet_name='new')
 print (df)
 df['c'] = df['a'] + df['b']
 print(df)
