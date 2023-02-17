c19939de369857bb3eefd9117c49e0d6200c332f
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-17 12:50:38 +0800

    Create How to create an array and add or delete a parameter.py

---
+++ How to create an array and add or delete a parameter.py
@@ -0,0 +1,17 @@
+Genes = ["RLK", "RLP", "CNL", 'TNL']
+Gene2 = "RLP"
+Gene3 = "CNL"
+Gene4 = 'TNL'
+
+x = Genes[3]
+print(x)
+x = Genes[1]
+print(x)
+
+#  Adding Array Elements
+Genes.append("TX")
+print(Genes)
+
+#  Removing Array Elements
+Genes.pop(2)
+print(Genes)
