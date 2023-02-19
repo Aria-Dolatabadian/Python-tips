208ee68d47c9897eac207e4a2e6964440a55f0a0
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-19 08:59:19 +0800

    Create And-Or in If statements.py

---
+++ And-Or in If statements.py
@@ -0,0 +1,11 @@
+a = 100
+b = 20
+c = 200
+if a > b and c > a:
+  print("Both conditions are True")
+
+a = 100
+b = 20
+c = 200
+if a > b or a > c:
+  print("At least one of the conditions is True")
