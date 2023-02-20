d41de2645ad50929968bdb46d3a743518be92d0e
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-20 13:19:42 +0800

    Create List items.py

---
+++ List items.py
@@ -0,0 +1,12 @@
+Genes = ["RLK", "RLP", "CNL", "CNL", "4"]
+print(Genes)
+print(len(Genes))
+print(type(Genes))
+print(Genes[3])
+print(Genes[-1])
+print(Genes[1:3])
+print(Genes[:3])
+print(Genes[2:])
+print(Genes[-4:-1])
+if "RLP" in Genes:
+  print("Yes")
