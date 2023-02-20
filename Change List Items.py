e0050762d7dbac3558d37780e6b4d31a441d1c4e
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-20 13:20:01 +0800

    Create Change List Items.py

---
+++ Change List Items.py
@@ -0,0 +1,14 @@
+Genes = ["RLK", "RLP", "CNL", "CNL", "4"]
+print(Genes)
+Genes[1] = "NBS"
+print(Genes)
+Genes[0:2] = ["TNL","TX"]
+print(Genes)
+Genes[1:3] = ["NBS","NL"]
+print(Genes)
+Genes[0:5] = ["RLP"]
+print(Genes)
+Genes.insert(1, "CNL")
+print(Genes)
+Genes.insert(1, "RLK")
+print(Genes)
