02574b4fa9d24fa6a54d40d8250f6ef3586d5d86
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-20 13:18:11 +0800

    Create String slicing.py

---
+++ String slicing.py
@@ -0,0 +1,15 @@
+# Get the characters from position 2 to position 5 (not included)
+DNA1 = "ATACGATACTGA"
+print(DNA1[2:5])
+
+# Slice From the Start
+RNA1 = "CAGAUAGACGAUAGACGAUA"
+print(RNA1[:5])
+
+# Slice To the End
+DNA2 = "ATATGATAGACATAGGAGA"
+print(DNA2[2:])
+
+# Negative Indexing
+RNA2 = "ACAGAUAGAUAGACAG"
+print(RNA2[-5:-2])
