385fc78a09dadbdd138240f13814f7f6e4316881
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-22 13:03:42 +0800

    Create Using a While Loop.py

---
+++ Using a While Loop.py
@@ -0,0 +1,14 @@
+#Print all items, using a while loop to go through all the index numbers:
+AvrGenes = ("AvrLm1", "AvrLm2", "AvrLm3")
+
+i = 0
+while i < len(AvrGenes):
+  print(AvrGenes[i])
+  i = i + 1
+print("................")
+
+
+i = 0
+while i <len(AvrGenes):
+    print(AvrGenes[2])
+    i = i + 1
