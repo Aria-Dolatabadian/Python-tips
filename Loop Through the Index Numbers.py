3d6b8bef9c00b7ce6411ecda72f3b5fe35939c3f
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-22 13:02:57 +0800

    Create Loop Through the Index Numbers.py

---
+++ Loop Through the Index Numbers.py
@@ -0,0 +1,9 @@
+AvrGenes = ("AvrLm1", "AvrLm2", "AvrLm3")
+for i in range(len(AvrGenes)):
+  print(AvrGenes[i])
+print("............")
+for i in range(len(AvrGenes)):
+  print(AvrGenes[0])
+print("............")
+for i in range(len(AvrGenes)):
+  print(AvrGenes[2])
