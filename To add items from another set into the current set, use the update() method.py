41f455bfc767c2304fb32e1ce948daf383059a77
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-23 09:56:53 +0800

    Update To add items from another set into the current set, use the update() method.py

--- To add items from another set into the current set, use the update() method.py
+++ To add items from another set into the current set, use the update() method.py
@@ -2,3 +2,11 @@ NBS = {"CNL", "TNL"}
 TM = {"RLK", "RLP"}
 NBS.update(TM)
 print(NBS)
+
+
+
+#This returns an error because it is impossible to add a set to a list but it is possible to add a list to a set.
+NBS = ["CNL", "TNL"]
+TM = {"RLK", "RLP"}
+NBS.update(TM)
+print(NBS)
