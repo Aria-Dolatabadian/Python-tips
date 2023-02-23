518d9ef6b358b0a9f6855b99450829a2b6f7d660
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-23 09:59:46 +0800

    Update Join Sets.py

--- Join Sets.py
+++ Join Sets.py
@@ -5,3 +5,13 @@ print(RGAs)
 #Or
 RGAs = NBS.union(TM)
 print(RGAs)
+
+#The update() method inserts the items in set2 into set1:
+
+TM = {"RLK", "RLP"}
+NBS ={"CNL","TNL"}
+TM.update(NBS)
+print(TM)
+#Or
+NBS.update(TM)
+print(NBS)
