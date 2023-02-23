4e2264fa66ee1bec8d81abe81a085f9af928c8b9
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-23 10:00:47 +0800

    Update Join Sets.py

--- Join Sets.py
+++ Join Sets.py
@@ -15,3 +15,19 @@ print(TM)
 #Or
 NBS.update(TM)
 print(NBS)
+
+#Keep ONLY the Duplicates
+
+#The intersection_update() method will keep only the items that are present in both sets.
+
+RGA1 = {"RLK", "RLP", "CNL", "TNL"}
+RGA2 = {"RLK", "RLP", "TX", "NL"}
+RGA1.intersection_update(RGA2)
+print(RGA1)
+
+#The intersection() method will return a new set, that only contains the items that are present in both sets.
+
+RGA1 = {"RLK", "RLP", "CNL", "TNL"}
+RGA2 = {"RLK", "RLP", "TX", "NL"}
+RGA3 = RGA1.intersection(RGA2)
+print(RGA3)
