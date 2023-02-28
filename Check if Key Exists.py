e9b735511f632c805c85a05b4969378cced8d330
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-02-28 11:39:49 +0800

    Create Check if Key Exists.py

---
+++ Check if Key Exists.py
@@ -0,0 +1,5 @@
+Gene = {"Class": "NBS-LRR","Type": "TNL","Size": 2500}
+if "Type" in Gene:
+  print("Yes, 'Type' is one of the keys in the Gene dictionary")
+else:
+   print("No, 'Type' is not one of the keys in the Gene dictionary")
