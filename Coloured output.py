292af2a282debc27ff6830980f1d6d43f7b9bec3
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-03-01 13:07:28 +0800

    Create Coloured output.py

---
+++ Coloured output.py
@@ -0,0 +1,8 @@
+from colorama import Fore
+print(Fore.GREEN+"GENE")
+print(Fore.RED+"GENE")
+print(Fore.BLUE+"GENE")
+print(Fore.CYAN+"GENE")
+print(Fore.BLACK+"GENE")
+print(Fore.YELLOW+"GENE")
+print(Fore.WHITE+"GENE")
