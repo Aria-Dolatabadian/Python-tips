ed3c890088e367cebaa2ead9b461d7e4c67a9ab3
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-03-03 09:11:14 +0800

    Update Use to_csv to export the data frame in Python.py

--- Use to_csv to export the data frame in Python.py
+++ Use to_csv to export the data frame in Python.py
@@ -1,1 +1,8 @@
 df.to_csv("data.csv", sep=",")
+
+penguins.to_csv("FileName.csv", index=False)
+
+#from numpy
+import numpy as np
+from numpy import savetxt
+savetxt('data.csv', data, delimiter=',')
