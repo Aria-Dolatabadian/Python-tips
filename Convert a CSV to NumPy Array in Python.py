cfbebc6c9635933000f0041e610cc9346a0a77f6
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-03-09 09:46:38 +0800

    Update Convert a CSV to NumPy Array in Python.py

--- Convert a CSV to NumPy Array in Python.py
+++ Convert a CSV to NumPy Array in Python.py
@@ -7,3 +7,12 @@ print(type(a))
 x = np.loadtxt('x.csv', delimiter=',')
 
 print(type(x))
+
+
+
+
+#You can convert a CSV file with first-line header to a NumPy array by calling np.loadtxt() with three arguments: 
+#the filename, skiprows=1 to skip the first line (header), and the delimiter string. 
+#For example, the expression
+#np.loadtxt('my_file.csv', skiprows=1, delimiter=',') 
+#returns a NumPy array from the 'my_file.csv' with delimiter symbols ',' while skipping the first line.
