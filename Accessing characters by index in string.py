2e8252b23304cfb59271ad7e5a19b76e240b7318
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-03-28 14:26:43 +0800

    Create Accessing characters by index in string.py

---
+++ Accessing characters by index in string.py
@@ -0,0 +1,5 @@
+Seq = "ATACAGATGACATAGAGATATATACAGAT"
+print( "Last character in string : " , Seq[-1] )
+print( "Second Last character in string : " , Seq[-2] )
+print( "First character in string : " , Seq[ -len(Seq) ] )
+print( "First character in string : " , Seq[0] )
