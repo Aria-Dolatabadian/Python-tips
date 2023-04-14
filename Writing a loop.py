914fd91a57b31559b6ae3ec870c615d96dae0a18
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 12:48:51 +0800

    Create Writing a loop.py

---
+++ Writing a loop.py
@@ -0,0 +1,9 @@
+Pathogens = ["Leptosphaeria maculans",
+             "Verticillium longisporum",
+             "Sclerotinia sclerotiorum",
+             "Erwinia carotovora"]
+
+print(Pathogens[0] + " is a fungus")
+print(Pathogens[1] + " is a fungus")
+print(Pathogens[2] + " is a fungus")
+print(Pathogens[3] + " is a bacterium")
