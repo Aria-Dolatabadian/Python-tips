bf8a8a2ce0f2c62f6e2143975651f12481847b95
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 12:47:04 +0800

    Create Proteins count.py

---
+++ Proteins count.py
@@ -0,0 +1,7 @@
+protein = "vlspadktnv"
+valine_count = protein.count('v')
+lsp_count = protein.count('lsp')
+tryptophan_count = protein.count('w')
+print("valines: " + str(valine_count))
+print("lsp: " + str(lsp_count))
+print("tryptophans: " + str(tryptophan_count))
