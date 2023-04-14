f7704ab9279e3939bc2f26b6a21b94fe21478162
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 12:28:48 +0800

    Create Average using input.py

---
+++ Average using input.py
@@ -0,0 +1,16 @@
+name = input ("Your Name: ")
+a= float(input ("Math: "))
+b= float (input("Physics: "))
+c= float(input("Python:  "))
+d = float(input("Biology: "))
+e= float(input("Biochemistry: "))
+f= float(input("Chemistry: "))
+g= float(input("Statistics: "))
+Sum = ( a+b+c+d+e+f+g)
+result = (Sum*100)
+Average= (result/700)
+print(Average)
+if Average >90 :
+ print("Congratulations " + name + " you passed")
+elif Average <90:
+ print("Sorry " + name + " you failed")
