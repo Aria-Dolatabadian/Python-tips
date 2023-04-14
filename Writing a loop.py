02e54b0e197eaa709204a382d9be9573edcc6556
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 12:50:00 +0800

    Update Writing a loop.py

--- Writing a loop.py
+++ Writing a loop.py
@@ -7,3 +7,12 @@ print(Pathogens[0] + " is a fungus")
 print(Pathogens[1] + " is a fungus")
 print(Pathogens[2] + " is a fungus")
 print(Pathogens[3] + " is a bacterium")
+
+
+#or
+
+DNA = ["GCTACAGACGACT", "ACTACAGACG", "CTTCCA"]
+for seq in DNA:
+    name_length = len(seq)
+    first_letter = seq[0]
+    print((seq + " starts with " + first_letter),("and has " + str(name_length) + " bases"))
