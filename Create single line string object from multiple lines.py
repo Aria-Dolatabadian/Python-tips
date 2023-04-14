9194addd114db880437a9878bf7b35ea8fd14bcf
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 12:08:22 +0800

    Create Create single line string object from multiple lines.py

---
+++ Create single line string object from multiple lines.py
@@ -0,0 +1,21 @@
+Seq1='''AAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAA'''
+print(Seq1)
+print('-----------------------')
+Seq2= ('TTTTTTT'
+'TTTTTTTTTT'
+'TTTTTTTTTTTT')
+print(Seq2)
+print('-----------------------')
+Seq3 = "CCCCCCCCCC " \
+         "CCCCCCCCCC " \
+         "CCCCCCCCCC"
+print(Seq3)
+print('-----------------------')
+Seq4 = ''.join((
+    "GGGGGGGGGG"
+    "GGGGGGGGGG"
+    "GGGGGGGGGG"
+))
+print(Seq4)
