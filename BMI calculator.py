1abbd431ec32e4f6f311966481ac5f15473944b8
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 12:19:38 +0800

    Update BMI calculator.py

--- BMI calculator.py
+++ BMI calculator.py
@@ -13,3 +13,24 @@ elif bmi > 35 :
     print('Your BMI is', bmi, 'which means you are extremely obese.')
 else:
     print('There is an error with your input')
+
+
+#or
+
+def test(x,y):
+    return round(x/(y*y),2)
+c=int(input("Weight (kg):"))
+d=float(input('Height (m):'))
+print('BMI:',test(c, d))
+if test(c,d) <= 18.5:
+    print('You are under weight')
+elif test(c,d) > 18.5 and test(c,d) < 25:
+     print('You are normal')
+elif test(c,d) > 25 and test(c,d) < 30:
+     print('You are overweight')
+elif test(c,d) > 30 and test(c,d) <35:
+     print('You are obese')
+elif test(c,d) > 35 :
+    print('You are extremely obese')
+else:
+    print('There is an error with your input')
