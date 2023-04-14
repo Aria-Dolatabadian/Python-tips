400cda8616dce77bdff1e49cf06f4bb3035d39cb
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 12:39:30 +0800

    Create How to create a csv table and format a specific value.py

---
+++ How to create a csv table and format a specific value.py
@@ -0,0 +1,18 @@
+import xlsxwriter
+data = {
+    "Gene":["Bna123","Bna124"],
+    "Class":["RLK","RLP"],
+    "Size":[100,150]
+}
+workbook = xlsxwriter.Workbook('d:\python\Genes_Table.xlsx')
+book_format = workbook.add_format(properties={'bold': True, 'font_color': 'red'})
+worksheet = workbook.add_worksheet('Gene')
+for col_num, header in enumerate(data.keys()):
+    worksheet.write(0,col_num, (header))
+for row_num,row_data in enumerate(zip(*data.values())):
+    for col_num, cell_data in enumerate(row_data):
+        if cell_data ==  "RLK":
+            worksheet.write(row_num+1, col_num, cell_data, book_format)
+        else:
+            worksheet.write(row_num+1, col_num, cell_data)
+workbook.close()
