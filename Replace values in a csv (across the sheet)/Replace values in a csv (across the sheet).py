55a2736c89686664360127c72ff2672f708311bd
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 12:38:42 +0800

    Update Replace values in a csv (across the sheet).py

--- Replace values in a csv (across the sheet)/Replace values in a csv (across the sheet).py
+++ Replace values in a csv (across the sheet)/Replace values in a csv (across the sheet).py
@@ -8,3 +8,11 @@ dataframe.replace(to_replace="RLP",
                   inplace=True)
 dataframe.to_csv('d:\python\change2.csv',
                  index=False)
+#or
+
+text = open("change.csv", "r")
+text = ''.join([i for i in text])
+text = text.replace("RLK", "NBS")
+x = open("d:\python\change2.csv", "w")
+x.writelines(text)
+x.close()
