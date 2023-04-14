2a405adaa141dae96996e8f45956cbfa59a254d7
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 12:29:05 +0800

    Create Alphabetical order.py

---
+++ Alphabetical order.py
@@ -0,0 +1,9 @@
+x = 'rice'
+k = input('Name a crop: ')
+
+if k < x:
+     print('Your crop ' + k + ' is before ' + x)
+elif k > x:
+     print('Your crop ' + k + ' is after ' +x)
+else:
+     print('Your crop ' + k + ' is equal to ' + x)
