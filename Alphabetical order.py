9474ecfe6eaefbea1a11cb019c96d03ea7c8d395
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 12:32:25 +0800

    Update Alphabetical order.py

--- Alphabetical order.py
+++ Alphabetical order.py
@@ -7,3 +7,21 @@ elif k > x:
      print('Your crop ' + k + ' is after ' +x)
 else:
      print('Your crop ' + k + ' is equal to ' + x)
+
+     
+     #or
+     
+     
+     x = 'rice'
+
+while True:
+    k = input('Name a crop (or enter "quit" to exit): ')
+
+    if k == 'quit':
+        break
+    elif k < x:
+        print('Your crop ' + k + ' is before ' + x)
+    elif k > x:
+        print('Your crop ' + k + ' is after ' + x)
+    else:
+        print('Your crop ' + k + ' is equal to ' + x)
