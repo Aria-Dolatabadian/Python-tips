c2eb359df2134dd74b177d962e6f043daaf1f8db
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-14 12:09:30 +0800

    Delete Compare two csv files and print out differences.py

--- Compare two csv files and print out differences.py
+++
@@ -1,8 +0,0 @@
-with open('1.csv', 'r') as t1, open('2.csv', 'r') as t2:
-    fileone = t1.readlines()
-    filetwo = t2.readlines()
-
-with open('Difference.csv', 'w') as outFile:
-    for line in filetwo:
-        if line not in fileone:
-            outFile.write(line)
