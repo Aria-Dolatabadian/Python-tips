ebdb423aba5014161ee2154f8b2bcb2d49629867
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-30 10:19:46 +0800

    Create Get the volume of a ellipsoid.py

---
+++ Get the volume of a ellipsoid.py
@@ -0,0 +1,6 @@
+r1 = float(input(' Please Enter the a axis of a Ellipsoid: '))
+r2 = float(input(' Please Enter the b axis of a Ellipsoid: '))
+r3 = float(input(' Please Enter the c axis of a Ellipsoid: '))
+pi = 3.1415926535897931
+V= 4/3*pi* r1*r2*r3
+print('The volume of the Ellipsoid is: ',V)
