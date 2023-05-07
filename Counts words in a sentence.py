59dcd2c0824ff8d27a2dd0a278f57e8f2117677f
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-05-07 09:00:45 +0800

    Create Counts words in a sentence.py

---
+++ Counts words in a sentence.py
@@ -0,0 +1,7 @@
+test_string = "canola is an oilseed crop"
+#original string
+print ("The original string is : " + test_string)
+# using split() function
+res = len(test_string.split())
+# total no of words
+print ("The number of words in string are : " + str(res))
