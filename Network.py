20065c25dc04959049df36b4e288ca834d078f0a
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-05-14 07:54:13 +0800

    Update Network.py

--- Network.py
+++ Network.py
@@ -13,3 +13,17 @@ G = nx.from_pandas_edgelist(df, 'from', 'to')
 # Pl
 nx.draw(G, with_labels=True)
 plt.show()
+
+#or
+
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+# Build a dataframe with your connections
+df = pd.DataFrame({'from': ['A', 'B', 'C', 'A'], 'to': ['D', 'A', 'E', 'C'], 'value': [1, 10, 5, 5]})
+# Build your graph
+G = nx.from_pandas_edgelist(df, 'from', 'to', create_using=nx.Graph())
+# Custom the nodes:
+nx.draw(G, with_labels=True, node_color='skyblue', node_size=1500, edge_color=df['value'], width=10.0,
+        edge_cmap=plt.cm.Blues)
+plt.show()
