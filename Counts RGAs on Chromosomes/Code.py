d232d01cc5aa435086f5293ee2b30e44a9b3f33e
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-06-02 14:50:57 +0800

    Create Code.py

---
+++ Counts RGAs on Chromosomes/Code.py
@@ -0,0 +1,10 @@
+import pandas as pd
+# Read the CSV file
+csv_file = 'data.csv'
+df = pd.read_csv(csv_file)
+# Count the number of each type on each chromosome
+count_df = df.groupby(['Chromosomes', 'Type']).size().reset_index(name='Count')
+# Display the count dataframe
+print(count_df)
+count_df.to_csv("RGA_count.csv", index=False)
+
