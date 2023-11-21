8467730abd3dc5bba377f14a9ec0cb09dd3eb90b
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-11-21 12:02:42 +0800

    Update Blood group compatibility.py

--- Blood group compatibility.py
+++ Blood group compatibility.py
@@ -23,3 +23,34 @@ if are_blood_types_compatible(person1_blood_type, person2_blood_type):
     print(f"The two individuals have compatible blood types for transfusion.")
 else:
     print(f"The two individuals have incompatible blood types for transfusion.")
+
+
+
+
+
+#or
+
+def find_compatible_blood_types(blood_group, rh_factor):
+    blood_compatibility = {
+        'A+': ['A+', 'AB+'],
+        'A-': ['A+', 'A-', 'AB+', 'AB-'],
+        'B+': ['B+', 'AB+'],
+        'B-': ['B+', 'B-', 'AB+', 'AB-'],
+        'AB+': ['AB+'],
+        'AB-': ['AB+', 'AB-'],
+        'O+': ['A+', 'B+', 'AB+', 'O+'],
+        'O-': ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
+    }
+
+    blood_type = blood_group + rh_factor
+    if blood_type in blood_compatibility:
+        compatible_blood_types = blood_compatibility[blood_type]
+        return compatible_blood_types
+    else:
+        return "Invalid blood group or Rh factor combination"
+
+blood_group = input("Enter blood group (e.g., A, B, AB, O): ").upper()
+rh_factor = input("Enter Rh factor (+ or -): ")
+
+result = find_compatible_blood_types(blood_group, rh_factor)
+print(f"Compatible blood types: {', '.join(result)}" if isinstance(result, list) else result)
