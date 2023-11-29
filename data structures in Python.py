f48e7453d35b53b31ba1cf7b25d2bd2abb10fb33
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-11-29 08:54:56 +0800

    Create data structures in Python.py

---
+++ data structures in Python.py
@@ -0,0 +1,22 @@
+my_list = [1, 2, "three", 4.0]
+
+my_tuple = (1, 2, "three", 4.0)
+
+my_set = {1, 2, 3, 4, 5}
+
+my_dict = {"name": "John", "age": 30, "city": "New York"}
+
+my_string = "Hello, World!"
+
+import numpy as np
+my_array = np.array([1, 2, 3, 4])
+
+from collections import deque
+my_queue = deque([1, 2, 3])
+
+# Using collections.deque
+from collections import deque
+my_stack = deque([1, 2, 3])
+
+# Using list
+my_stack_list = [1, 2, 3]
